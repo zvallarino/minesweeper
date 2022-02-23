0f1c45cc3d8e18467fd5b6986459eceb41ce8dc0
Zachary <zvcoding@gmail.com>
2022-02-23 14:32:53 -0500

    we back to where we were but with class components

--- src/components/ATwo.js
+++ src/components/ATwo.js
@@ -11,7 +11,52 @@ class ATwo extends React.Component{
 
   componentDidMount = () => {
     console.log(this)
+    console.log("lol")
+    this.numberOfBombsPerSquare(this.divRef.current)
+    console.log(this.divRef.current)
+    console.log("lol")
   }
+
+  arrayOfClick = (x,y) =>{
+    const left = x - 1;
+    const middleh = x;
+    const right = x + 1;
+    
+    const top = String.fromCharCode(97 + y-1);
+    const middlev = String.fromCharCode(97 + y);
+    const bottom = String.fromCharCode(97 + y+1);
+
+
+
+    const clickArray = [
+      [`${left}${top}`],[`${middleh}${top}`],[`${right}${top}`],
+      [`${left}${middlev}`],[`${right}${middlev}`],
+      [`${left}${bottom}`],[`${middleh}${bottom}`],[`${right}${bottom}`],
+    ]
+
+    // Removed middleV,middleH so it doesnt count itself
+
+   return clickArray
+  }
+
+
+  numberOfBombsPerSquare = (refObject) => {
+    for (const property in refObject) {
+  // console.log(`${property}: ${refObject[property].i}`);
+  const clickCircle = this.arrayOfClick(refObject[property].i,refObject[property].j)
+  // console.log(clickCircle)
+  let counter = 0;
+  for(let i = 0;i<clickCircle.length;i++){
+      if(refObject[`${clickCircle[i][0]}`]?.isABomb === "BOMB"){
+        counter +=1;
+      }
+  }
+  refObject[property].numberOfBombs = counter
+  console.log(refObject[property].numberOfBombs)
+  }
+  }
+
+  
 
   randomNumber = (x) => {
     return Math.floor((Math.random() * x));
--- src/components/BTwo.js
+++ src/components/BTwo.js
@@ -24,39 +24,18 @@ class BTwo extends React.Component{
     this.props.refs.current[this.convertToIndentifier()].clicked = this.state.clicked
     this.props.refs.current[this.convertToIndentifier()].randomNumber = this.props.randomNumber
     this.props.refs.current[this.convertToIndentifier()].isABomb = this.isABomb()
+    this.props.refs.current[this.convertToIndentifier()].numberOfBombs = 1;
   }
 
-  arrayOfClick = () =>{
-    const left = this.props.i - 1;
-    const middleh = this.props.i;
-    const right = this.props.i + 1;
-    
-    const top = String.fromCharCode(97 + this.props.j-1);
-    const middlev = String.fromCharCode(97 + this.props.j);
-    const bottom = String.fromCharCode(97 + this.props.j+1);
-
-    console.log(typeof(top))
-
-    let clickArray = [
-      [`${left}${top}`],[`${middleh}${top}`],[`${right}${top}`],
-      [`${left}${middlev}`],[`${middleh}${middlev}`],[`${right}${middlev}`],
-      [`${left}${bottom}`],[`${middleh}${bottom}`],[`${right}${bottom}`],
-    ]
-
-    console.log(clickArray)
-  }
 
 
 
   componentDidMount = () => {
-    console.log('this just fired 2')
     this.returnThis()
-    console.log('this just fired 2')
   }
 
   handleClick = () =>{
     this.setState({backgroundColor:"blue"})
-    this.arrayOfClick()
   }
 
 
@@ -69,10 +48,11 @@ class BTwo extends React.Component{
   isABomb = () =>{
     if(this.props.randomNumber > 2){
         return "BOMB"
-    } else {
-        return this.convertToIndentifier()
-    }
+    } 
 }
+
+
+
 
   
 
@@ -87,7 +67,7 @@ class BTwo extends React.Component{
               style={
                 { height : this.props.h, width: this.props.w, backgroundColor:this.state.backgroundColor }}
         >
-        Yo this be crazy
+        {this.props.refs.current?.[this.convertToIndentifier()]?.isABomb==="BOMB"?"BOMB":this.props.refs.current?.[this.convertToIndentifier()]?.numberOfBombs}
         </button>
       </>
     );
